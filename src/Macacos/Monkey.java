a2e45a4af4d2d6e4e61484d53d0950439b16336d
motadianas <mota.dianas@gmail.com>
2025-09-15 13:48:31 -0300

    Melhoria de package macaco - monkey

--- src/Macacos/Monkey.java
+++ src/Macacos/Monkey.java
@@ -15,40 +15,56 @@ public class Monkey {
 
     //Métodos
     public void comer(String alimento) { // Falta a verificação de estomago cheio
+        boolean estomagoCheio = true;
         for (int i = 0; i < estomago.length; i++) {
             if (estomago[i] == null) {
+                estomagoCheio = false;
                 estomago[i] = alimento;
                 break;
             }
         }
+        if(estomagoCheio){
+            System.out.println("O estomago já está cheio. Não e possivel comer.");
+        }
     }
-    public String[] verEstomago() { // Falta adicionar a verificação de estomago vazio
-        int j = 0;
+    public void verEstomago() { // Falta adicionar a verificação de estomago vazio
+        int itemEstomago = 0;
         for (int i = 0; i < estomago.length; i++) {
             if (estomago[i] != null) {
-                j++;
+                itemEstomago++;
             }
         }
-        String[] estomagoAgora = new String[j];
-        for (int i = 0; i < j; i++) {
-            estomagoAgora[i]= estomago[i];
+        if(itemEstomago == 0){
+            System.out.println("O estomago de "+this.nome+" esta vazio agora. Vamos alimentá-lo?");
         }
-        return estomagoAgora;
+        else {
+            String[] estomagoAgora = new String[itemEstomago];
+            for (int i = 0; i < itemEstomago; i++) {
+                estomagoAgora[i]= estomago[i];
+            }
+            System.out.println("Dentro do estomago de "+this.nome+" ha: "+Arrays.toString(estomagoAgora)+".");
+        }
     }
+
     public void digerir(){ // Falta adicionar a verificação de estomago vazio
-        int j = 0;
+        int itemEstomago = 0;
         for (int i = 0; i < estomago.length; i++) {
             if (estomago[i] != null) {
-                j++;
+                itemEstomago++;
             } else {
                 break;
             }
         }
-        for(int i=0; i<j;i++){
-            if(i<estomago.length-1) {
-                estomago[i] = estomago[i + 1];
-            }else if(i==estomago.length-1){
-                estomago[i]=null;
+        if(itemEstomago==0){
+            System.out.println("O estomago de "+this.nome+" esta vazio agora. Vamos alimentá-lo?");
+        }
+        else{
+            for(int i=0; i<itemEstomago;i++){
+                if(i<estomago.length-1) {
+                    estomago[i] = estomago[i + 1];
+                }else if(i==estomago.length-1){
+                    estomago[i]=null;
+                }
             }
         }
     }
