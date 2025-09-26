d7ba16432aa8a13721c279e080b77e049b0a477b
Diana S. Mota <86123050+motadianas@users.noreply.github.com>
2025-09-25 21:22:32 -0300

    Update Carro.java

--- src/Abstracao/Carro.java
+++ src/Abstracao/Carro.java
@@ -1,31 +1,20 @@
 package Abstracao;
 
-public class Carro extends Veiculo implements Motorizado{
-    public Carro (int velocidade){
-        super(velocidade);
-    }
+public class Carro extends Veiculo /*implements Motorizado*/{
+    
     @Override
     public void acelerar(){
-        if(velocidade<80){
-            this.velocidade++;
-            System.out.println("O carro acelerou. | Velocidade = "+this.velocidade);
-        }else {
-            System.out.println("A velocidade máxima do veículo é controlada. Ele só chega até 80km/h");
-        }
+        super.velocidade+=10;
+        System.out.println("O carro está acelerando + 10 Km/h");
     }
     @Override
     public void frear(){
-        if(velocidade>0){
-            this.velocidade--;
-        System.out.println("O Carro freou. | Velocidade = "+this.velocidade);
-        }
-        else {
-            System.out.println("A velocidade já está muito baixa. Não dá para diminuir.");
-        }
+        super.velocidade -=10;
+        System.out.println("O Carro está freando -10 Km/h");
     }
-    @Override
-    public void abastecer(){
-        System.out.println("Carro abastecido");
-    }
+//    @Override
+//    public void abastecer(){
+//        System.out.println("Carro abastecido");
+//    }
 
 }
