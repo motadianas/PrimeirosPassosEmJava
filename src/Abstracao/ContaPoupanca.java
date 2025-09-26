3853f49e8bee07623c605f1e1867e97b0f1ef27d
Diana S. Mota <86123050+motadianas@users.noreply.github.com>
2025-09-25 21:23:48 -0300

    Update ContaPoupanca.java

--- src/Abstracao/ContaPoupanca.java
+++ src/Abstracao/ContaPoupanca.java
@@ -1,33 +1,29 @@
 package Abstracao;
 
 public class ContaPoupanca extends ContaBancaria{
-    protected ContaPoupanca(float saldoInicial){
-        super(saldoInicial);
-    }
-    @Override
-    public void sacar(float retirada) {
-        if(retirada>0){
-            if(saldo>=retirada){
-                saldo-=retirada;
-                System.out.println("Valor retirado da Conta Poupança: R$"+retirada);
-            }
-            else{
-                System.out.println("Saldo insuficiente na Conta Poupança para a retirada");
-            }
-        }
-        else {
-            System.out.println("Valor de retirada inválido.");
-        }
+    private double jurosDeposito = 0.01;
+
+    public ContaPoupanca(String titular, double saldo) {
+        super(titular, saldo);
     }
 
     @Override
-    public void depositar(float deposito) {
-        if(deposito>0){
-            saldo+=(deposito*1.1);
-            System.out.println("Valor depositado na Conta Poupança: R$" + deposito);
+    public void sacar(double valor) {
+        if(getSaldo()>=valor){
+            setSaldo(getSaldo()-valor);
+            System.out.println("Saque de R$ "+String.format("%.2f", valor)+
+                " realizado na Conta Poupanca de "+getTitular());
         }
-        else {
-            System.out.println("Valor de deposito inválido.");
-        }
+        else
+            System.out.println("Saldo insuficiente.");
     }
+
+    @Override
+    public void depositar(double valor){
+        double valorDeposito = getSaldo()+(valor+(valor*jurosDeposito));
+        setSaldo(valorDeposito);
+        System.out.println("Deposito de R$ "+String.format("%.2f", valor)+
+                " realizado na Conta Poupanca de "+getTitular());
+    }
+    
 }
