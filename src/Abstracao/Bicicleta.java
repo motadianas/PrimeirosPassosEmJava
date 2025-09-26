82a3da1a7729877ebb6e0022b00161d69f8709b9
Diana S. Mota <86123050+motadianas@users.noreply.github.com>
2025-09-25 21:20:57 -0300

    Update Bicicleta.java

--- src/Abstracao/Bicicleta.java
+++ src/Abstracao/Bicicleta.java
@@ -1,23 +1,15 @@
 package Abstracao;
 
 public class Bicicleta extends Veiculo {
-    public Bicicleta(int velocidade) {
-        super(velocidade);
-    }
-
+    
     @Override
-    public void acelerar() {
-        this.velocidade++;
-        System.out.println("A bicicleta acelerou. | Velocidade = "+this.velocidade);
+    public void acelerar(){
+        super.velocidade+=2;
+        System.out.println("Voce esta pedalando");
     }
-
     @Override
-    public void frear() {
-        if (velocidade > 0) {
-            this.velocidade--;
-            System.out.println("A bicicleta freou. | Velocidade = "+this.velocidade);
-        } else {
-            System.out.println("A velocidade já está muito baixa. Não dá para diminuir.");
-        }
+    public void frear(){
+        super.velocidade-=2;
+        System.out.println("A bicicleta esta gastando havainas e diminuindo 2Km/h");
     }
 }
